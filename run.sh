5e862f44504eab3476da90da6f363b1cae113902
FuaerCN <FuaerCN@Gmail.com>
2020-12-30 13:08:40 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -26,9 +26,9 @@ Listen = :${PORT}
 ; 是否开启 Debug
 Debug = false
 ; Session 密钥, 一般在首次启动时自动生成
-SessionSecret = 23333
+SessionSecret = oXIYVNP23Z9cXRCqn8C8z8etztNo5E33Qd6n6q3TFe3Z7aPPSer4nOEmADwljdIh
 ; Hash 加盐, 一般在首次启动时自动生成
-HashIDSalt = something really hard to guss
+HashIDSalt = 2je20slhtLqQXalz7a0mCA9hU0H88B0D7vy3jiW4Zw97z35VBeglaFuDBEwmNma9
 
 [Redis]
 Server = ${REDIS_URL##*@}
